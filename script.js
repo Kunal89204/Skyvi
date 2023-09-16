c157663bbf8f28e194c7c2c2995fc17d3ff22dea
Kunal89204 <kunalkhandelwal108@gmail.com>
2023-09-16 18:07:06 +0530

    almost complete

---
+++ script.js
@@ -0,0 +1,36 @@
+let input = document.getElementById("input").value.trim();
+let search = document.getElementById("search")
+let city = document.querySelector(".city-name")
+let humidity = document.getElementById("humidity")
+let speed = document.getElementById("wind-speed")
+let temp = document.querySelector(".temp")
+
+
+search.addEventListener("click", function(){
+    if (search !== "") {
+        apiUrl = `https://api.weatherapi.com/v1/current.json?key=bc6cdf98ec47468598142603231609&q=${input}`
+
+fetch(apiUrl)
+.then(respo => respo.json())
+.then((data) => {
+    city.textContent = "";
+  humidity.textContent = "";
+  speed.textContent = "";
+  temp.textContent = "";
+  input.textContent = "";
+
+
+
+    city.textContent = data.location.name;
+    humidity.textContent = data.current.humidity;
+    speed.textContent = data.current.wind_kph
+    temp.textContent = data.current.temp_c;
+}).catch((error) =>{
+    console.log("there was an error")
+})
+
+
+
+    }
+})
+
