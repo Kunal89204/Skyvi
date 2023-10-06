e5073b2bd00f2c2adee3517720f9c74de660d06d
Kunal89204 <kunalkhandelwal108@gmail.com>
2023-10-06 18:47:25 +0530

    code fixed

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-let input = document.getElementById("input").value.trim();
-let search = document.getElementById("search")
-let city = document.querySelector(".city-name")
-let humidity = document.getElementById("humidity")
-let speed = document.getElementById("wind-speed")
-let temp = document.querySelector(".temp")
+let search = document.getElementById("search");
+let city = document.querySelector(".city-name");
+let humidity = document.getElementById("humidity");
+let speed = document.getElementById("wind-speed");
+let temp = document.querySelector(".temp");
 
+search.addEventListener("click", function() {
+  let input = document.getElementById("input").value.trim();
+  if (input !== "") {
+    let apiUrl = `https://api.weatherapi.com/v1/current.json?key=bc6cdf98ec47468598142603231609&q=${input}`;
 
-search.addEventListener("click", function(){
-    if (search !== "") {
-        apiUrl = `https://api.weatherapi.com/v1/current.json?key=bc6cdf98ec47468598142603231609&q=${input}`
-
-fetch(apiUrl)
-.then(respo => respo.json())
-.then((data) => {
-
-
-
-
-    city.textContent = data.location.name;
-    humidity.textContent = data.current.humidity;
-    speed.textContent = data.current.wind_kph
-    temp.textContent = data.current.temp_c;
-}).catch((error) =>{
-    console.log("there was an error")
-})
-
-
-
-    }
-})
-
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        city.textContent = data.location.name;
+        humidity.textContent = data.current.humidity;
+        speed.textContent = data.current.wind_kph;
+        temp.textContent = data.current.temp_c;
+      })
+      .catch((error) => {
+        console.log("There was an error:", error);
+      });
+  }
+});
