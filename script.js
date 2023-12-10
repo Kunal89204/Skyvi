1aac60f92d495fdfe22499ad9cef761e6a4a6919
Kunal89204 <kunalkhandelwal108@gmail.com>
2023-12-10 17:41:07 +0530

    added dyanmic images and error handling

--- script.js
+++ script.js
@@ -1,24 +1,68 @@
-let search = document.getElementById("search");
-let city = document.querySelector(".city-name");
-let humidity = document.getElementById("humidity");
-let speed = document.getElementById("wind-speed");
-let temp = document.querySelector(".temp");
+// Function to fetch weather data and update UI
+function updateWeather(cityName) {
+  const apiKey = 'bc6cdf98ec47468598142603231609';
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}`;
 
-search.addEventListener("click", function() {
-  let input = document.getElementById("input").value.trim();
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Update UI elements with weather data
+      document.querySelector(".city-name").textContent = data.location.name;
+      document.getElementById("humidity").textContent = data.current.humidity;
+      document.getElementById("wind-speed").textContent = data.current.wind_kph;
+      document.querySelector(".temp").innerHTML = `${data.current.temp_c}<sup>o</sup>C`;
+
+      const climate = data.current.condition.text;
+      console.log(climate);
+
+      // Update image based on weather condition
+      const img = document.getElementById("image");
+      if (climate.toLowerCase().includes("clear")) {
+        img.src = "icons/clear.png";
+      } else if (climate.toLowerCase().includes("sunny")) {
+        img.src = "/icons/sun.png";
+      } else if (climate.toLowerCase().includes("partly cloudy")) {
+        img.src = "/icons/partly-cloudy.png";
+      } else if (climate.toLowerCase().includes("snow")) {
+        img.src = "/icons/snowman.png";
+      } else if (climate.toLowerCase().includes("cloud")) {
+        img.src = "/icons/cloudy.png";
+      } else if (climate.toLowerCase().includes("mist")) {
+        img.src = "/icons/mist.png";
+      }
+    })
+    .catch((error) => {
+      console.log("There was an error:", error);
+      const errorbox = document.querySelector(".error");
+      errorbox.style.opacity = "1";
+
+      setTimeout(() => {
+        errorbox.style.opacity = "0";
+      }, 2000);
+    });
+}
+
+// Function to handle the search button click
+function handleSearch() {
+  const input = document.getElementById("input").value.trim();
   if (input !== "") {
-    let apiUrl = `https://api.weatherapi.com/v1/current.json?key=bc6cdf98ec47468598142603231609&q=${input}`;
+    updateWeather(input);
+  } else {
+    console.log("the input field can't be empty");
+    const emptyBox = document.querySelector(".empty-box");
+    emptyBox.style.opacity = "1";
 
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        city.textContent = data.location.name;
-        humidity.textContent = data.current.humidity;
-        speed.textContent = data.current.wind_kph;
-        temp.innerHTML = `${data.current.temp_c}<sup>o</sup>C`;
-      })
-      .catch((error) => {
-        console.log("There was an error:", error);
-      });
+    setTimeout(() => {
+      emptyBox.style.opacity = "0";
+    }, 2000);
   }
+}
+
+// Call the function to fetch weather data for a default location when the page loads
+window.addEventListener("load", function () {
+  const defaultCity = "India"; // You can set your desired default city
+  updateWeather(defaultCity);
 });
+
+// Attach the handleSearch function to the search button click event
+document.getElementById("search").addEventListener("click", handleSearch);
